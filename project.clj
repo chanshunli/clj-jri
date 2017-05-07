0dc81e335a44147f63ebf94cc6c90c86c84a0fba
chanshunli <chanshunli@gmail.com>
2017-05-07 09:49:43 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/fanannan/clj-jri"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :resource-paths [;"./lib/RserveEngine.jar"
-                   "./lib/REngine.jar"
-                   "./lib/JRI.jar"
-                   "./lib/libjri.so"
-                   ]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :resource-paths ["lib/JRI.jar"
+                   "lib/JRIEngine.jar"
+                   "lib/REngine.jar"
+                   "lib/libjri.jnilib"]
+  :jvm-opts [~(str "-Djava.library.path=/usr/local/lib/R/3.4/site-library/rJava/jri/:" (System/getProperty "java.library.path"))]
   :main clj-jri.core)
 
 ;lein clean; lein deps; lein compile; lein jar; lein uberjar; lein localrepo install ./target/clj-jri-0.0.1-standalone.jar clj-jri 0.0.1-standalone;
