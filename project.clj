f994c8c8612f786855e2dfae2bb3f13f3dc57e83
chanshunli <chanshunli@gmail.com>
2017-05-10 07:27:42 +0800

    使用系统的绝对路径

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject clj-jri "0.1.0-SNAPSHOT"
+(defproject clj-jri "0.1.1-SNAPSHOT"
   :description "Wrapper for Java/R interface"
   :url "https://github.com/fanannan/clj-jri"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :resource-paths ["lib/JRI.jar"
-                   "lib/JRIEngine.jar"
-                   "lib/REngine.jar"
-                   "lib/libjri.jnilib"]
+  :resource-paths ["/usr/local/lib/R/3.4/site-library/rJava/jri/JRI.jar"
+                   "/usr/local/lib/R/3.4/site-library/rJava/jri/JRIEngine.jar"
+                   "/usr/local/lib/R/3.4/site-library/rJava/jri/REngine.jar"
+                   "/usr/local/lib/R/3.4/site-library/rJava/jri/libjri.jnilib"]
   :jvm-opts [~(str "-Djava.library.path=/usr/local/lib/R/3.4/site-library/rJava/jri/:" (System/getProperty "java.library.path"))]
   :main clj-jri.core
   :aot :all)
